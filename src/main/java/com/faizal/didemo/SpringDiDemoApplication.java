cf770847c3e7ddf751d32842613290dfacf3aa6b
Faizal <zafri_faizal@outlook.com>
2019-01-27 01:26:41 +0530

    added context example

--- src/main/java/com/faizal/didemo/SpringDiDemoApplication.java
+++ src/main/java/com/faizal/didemo/SpringDiDemoApplication.java
@@ -2,13 +2,18 @@ package com.faizal.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.faizal.didemo.controller.MyController;
 
 @SpringBootApplication
 public class SpringDiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDiDemoApplication.class, args);
+
+		ApplicationContext apct = SpringApplication.run(SpringDiDemoApplication.class, args);
+		MyController controller = (MyController) apct.getBean("myController");
+		controller.hello();
 	}
 
 }
-
