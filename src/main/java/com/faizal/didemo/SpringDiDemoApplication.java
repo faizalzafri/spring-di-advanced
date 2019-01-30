fbdf74823edee51a68d483105ac22cf69c8c88fe
Faizal <zafri_faizal@outlook.com>
2019-01-30 20:39:32 +0530

    component-scanning

--- src/main/java/com/faizal/didemo/SpringDiDemoApplication.java
+++ src/main/java/com/faizal/didemo/SpringDiDemoApplication.java
@@ -3,13 +3,16 @@ package com.faizal.didemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.faizal.didemo.controller.ConstructorInjectedController;
-import com.faizal.didemo.controller.SetterInjectedController;
 import com.faizal.didemo.controller.MyController;
 import com.faizal.didemo.controller.PropertyInjectedController;
 
+import anothercontroller.SetterInjectedController;
+
 @SpringBootApplication
+@ComponentScan(basePackages= {"anothercontroller","com.faizal.didemo"})
 public class SpringDiDemoApplication {
 
 	public static void main(String[] args) {
